f412ca1e1479fedc416826d2c06e235ff7021ea4
Jamie Magee <jamie.magee@gmail.com>
2018-11-28 22:09:54 +0100

    Add UseBasicParsing flag

--- zulu8/update.ps1
+++ zulu8/update.ps1
@@ -5,14 +5,14 @@ $releases = 'https://www.azul.com/downloads/zulu/zulu-windows/'
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $url64   = $download_page.links | ? href -match '^.*jdk8.*.msi$' | % href | select -First 1
     $version = Split-Path $url -Leaf | Select-String -Pattern '^zulu8\.\d+\.\d+' | foreach {$_.Matches.Value}
