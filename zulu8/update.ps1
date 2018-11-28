fd5a0952b95eb9cf0b0fbb788b95abf88f43d51c
Jamie Magee <jamie.magee@gmail.com>
2018-11-28 16:18:03 +0100

    Initial zulu8

---
+++ zulu8/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'https://www.azul.com/downloads/zulu/zulu-windows/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $url64   = $download_page.links | ? href -match '^.*jdk8.*.msi$' | % href | select -First 1
+    $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+
+    @{
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+update
