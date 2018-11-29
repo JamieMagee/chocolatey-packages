eb418639ac4effafc6c1b1ea4fa854d98d8f226b
Jamie Magee <jamie.magee@gmail.com>
2018-11-29 16:27:54 +0100

    Initial zulu6 package

---
+++ zulu6/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu6'
+  fileType      = 'msi'
+  url64bit      = ''
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 6*'
+}
+
+Install-ChocolateyPackage @packageArgs
