029452824395bce0c4443942fa95a040069588f7
Jamie Magee <jamie.magee@gmail.com>
2018-11-29 16:42:23 +0100

    Initial Zulu 10 package

---
+++ zulu10/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu10'
+  fileType      = 'msi'
+  url64bit      = ''
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 10*'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ zulu10/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'https://www.azul.com/downloads/zulu/zulu-windows/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url64   = $download_page.links | ? href -match '^.*jdk10.*.msi$' | % href | select -First 1
+    $version = (Split-Path $url64 -Leaf | Select-String -Pattern '^zulu10((\.\d+)+)').Matches.Value.Substring(4)
+
+    @{
+        URL64   = $url64
+        Version = $version
+    }
+}
+
+update -ChecksumFor 64
