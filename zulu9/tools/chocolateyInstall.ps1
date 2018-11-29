a57b05101d7d5fa6c1827f56cd5ada9866fa1176
Jamie Magee <jamie.magee@gmail.com>
2018-11-29 16:40:29 +0100

    Initial Zulu 9 package

---
+++ zulu9/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu9'
+  fileType      = 'msi'
+  url64bit      = ''
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 9*'
+}
+
+Install-ChocolateyPackage @packageArgs
