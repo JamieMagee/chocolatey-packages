007fa636a718ba8ae1d20efb37be7baff858665d
Jamie Magee <jamie.magee@gmail.com>
2018-11-29 16:35:28 +0100

    Initial Zulu 7 package

---
+++ zulu7/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu7'
+  fileType      = 'msi'
+  url64bit      = ''
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 7*'
+}
+
+Install-ChocolateyPackage @packageArgs
