beb1b99b8f39fd92024e3380143889c3eefad978
Chocolatey <chocolatey@realdimensions.net>
2018-11-29 15:43:58 +0000

    AU: 1 updated - zulu10 [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/7be7aa754a1a0764a2e46adfb2f54f7fdd1c1747

--- zulu10/tools/chocolateyInstall.ps1
+++ zulu10/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu10'
   fileType      = 'msi'
-  url64bit      = ''
-  checksum64    = ''
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu10.3+5-jdk10.0.2-win_x64.msi'
+  checksum64    = '4b8d1238aa911706065c370b65b92e4b87a57189a7ad944c369770e0fc07bcca'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
