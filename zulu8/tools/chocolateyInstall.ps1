9c8405b3ab9c19d955d76263323f9873306a516d
Jamie Magee <jamie.magee@gmail.com>
2018-11-29 11:21:07 +0100

    double quotes

--- zulu8/tools/chocolateyInstall.ps1
+++ zulu8/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = 'Zulu8'
+  packageName   = 'zulu8'
   fileType      = 'msi'
   url64bit      = 'https://cdn.azul.com/zulu/bin/zulu8.33.0.1-jdk8.0.192-win_x64.msi'
   checksum64    = 'f60b3bfe0a4923aedca8a657ee88cf452fd7da2bac79e49df2fbd67506ef19d7'
   checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart"
+  silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
   softwareName  = 'Zulu 8*'
 }
