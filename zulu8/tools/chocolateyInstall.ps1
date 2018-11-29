7d51005e84af5438799347c36dfe01f749e8c19f
Jamie Magee <JamieMagee@users.noreply.github.com>
2018-11-29 15:48:57 +0100

    Initial zulu8 (#1)

---
+++ zulu8/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu8'
+  fileType      = 'msi'
+  url64      = 'https://cdn.azul.com/zulu/bin/zulu8.33.0.1-jdk8.0.192-win_x64.msi'
+  checksum64    = 'f60b3bfe0a4923aedca8a657ee88cf452fd7da2bac79e49df2fbd67506ef19d7'
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 8*'
+}
+
+Install-ChocolateyPackage @packageArgs
