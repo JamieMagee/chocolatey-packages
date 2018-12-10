fd0aab2f95f37346b6a715c456dae7a4f0b19b7e
Jamie Magee <jamie.magee@gmail.com>
2018-12-10 09:00:30 +0100

    / -> \

--- jetty/tools/chocolateyInstall.ps1
+++ jetty/tools/chocolateyInstall.ps1
@@ -13,4 +13,4 @@ Install-ChocolateyZipPackage @packageArgs
 $zip = Split-Path $packageArgs.url -Leaf
 $dir = $zip.Substring(0, $zip.Length - 4)
 
-Install-ChocolateyEnvironmentVariable 'JETTY_HOME' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)/$dir"
+Install-ChocolateyEnvironmentVariable 'JETTY_HOME' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\$dir"
