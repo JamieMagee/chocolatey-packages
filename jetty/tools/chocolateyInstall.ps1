31a927f675d32bdd531df57465c2e069a5fb23cb
Jamie Magee <jamie.magee@gmail.com>
2018-12-10 09:28:10 +0100

    Remove version

--- jetty/tools/chocolateyInstall.ps1
+++ jetty/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = 'Stop';
 $packageArgs = @{
   packageName    = 'jetty'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  url            = 'https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.4.14.v20181114/jetty-distribution-9.4.14.v20181114.zip'
-  checksum       = 'd349be63775a3e252719d2a63ae271f066208b210d52bf2fc044d43dcd367476'
+  url            = ''
+  checksum       = ''
   checksumType   = 'sha256'
 }
 
