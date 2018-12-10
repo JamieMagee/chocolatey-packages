4f4e27794e3dbcf33e6931e2533baf66f4b8d31e
Jamie Magee <jamie.magee@gmail.com>
2018-12-10 08:59:33 +0100

    Add zipFileName

--- jetty/tools/chocolateyUninstall.ps1
+++ jetty/tools/chocolateyUninstall.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName = 'jetty'
+  zipFileName = 'jetty-distribution-9.4.14.v20181114.zip'
 }
 
 Uninstall-ChocolateyZipPackage @packageArgs
--- jetty/update.ps1
+++ jetty/update.ps1
@@ -5,8 +5,11 @@ $releases = 'https://www.eclipse.org/jetty/download.html'
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"   = "`$1'$($Latest.URL)'"
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum)'"
+    }
+    ".\tools\chocolateyUninstall.ps1" = @{
+        "(?i)(^\s*zipFileName\s*=\s*)('.*')" = "`$1'$($Latest.ZIP)'"
     }
   }
 }
@@ -14,13 +17,14 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-  $url = $download_page.links | ? href -match '^.*.zip$' | % href | select -First 1
+  $url = $download_page.links | Where-Object href -match '^.*.zip$' | ForEach-Object href | Select-Object -First 1
   $zip = ((Split-Path $url -Leaf) -split "-")[2]
   $version = $zip.Substring(0, $zip.Length - 4).Replace('v', '')
 
   @{
     URL   = $url
     Version = $version
+    ZIP = $zip
   }
 }
 
