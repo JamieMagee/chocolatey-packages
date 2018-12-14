9c4a93a26b6bbef93090d9d089f35662fa41b6b5
Jamie Magee <jamie.magee@gmail.com>
2018-12-14 10:06:36 +0100

    Try without UseBasicParsing

--- jetty/update.ps1
+++ jetty/update.ps1
@@ -12,8 +12,8 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
+  $download_page = Invoke-WebRequest -Uri $releases
+  
   $url = $download_page.links | Where-Object href -match '^.*.zip$' | ForEach-Object href | Select-Object -First 1
   $zip = ((Split-Path $url -Leaf) -split "-")[2]
   $version = $zip.Substring(0, $zip.Length - 4).Replace('v', '')
