842a780b9b7e2ead4984b69694da41b22ec8cae2
Chocolatey <chocolatey@realdimensions.net>
2019-01-27 16:28:07 +0000

    AU: 1 updated - zulu [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/4a1d3a4c55747f90a197a3017b2bda89e5832caa

--- zulu/tools/chocolateyInstall.ps1
+++ zulu/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu11.2.3-jdk11.0.1-win_x64.msi'
-  checksum64    = '893b8b0b4b63be62296dfeaa7922a0127e533b8dde6533a67678fa9f2e88b1a1'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu11.29.3-ca-jdk11.0.2-win_x64.msi'
+  checksum64    = '5aab468aefc490d94a0142f5c7ece62b94bf139dac082d4a667b50875e48e51c'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
   validExitCodes= @(0)
