a1668bef52a03cc5bc233c906faa147385ad752e
Jamie Magee <jamie.magee@gmail.com>
2019-03-29 15:34:29 +0100

    Push without URL, checksum, or version to force version update

--- zulu12/tools/chocolateyInstall.ps1
+++ zulu12/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu12'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu12.1.3-ca-jdk12-win_x64.msi'
-  checksum64    = '9556f4e65e9b54ea44b27c9d86cc14e0cdada15e85237671345c38a80018e4a4'
+  url64bit      = ''
+  checksum64    = ''
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
