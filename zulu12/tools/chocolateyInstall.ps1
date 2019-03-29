953c8c889626c3edf210d35ed5b50c2e182cfcd0
Jamie Magee <jamie.magee@gmail.com>
2019-03-29 15:28:19 +0100

    Update zulu package. Add zulu12 package
    Fixes #5

--- zulu12/tools/chocolateyInstall.ps1
+++ zulu12/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
-  packageName   = 'zulu'
+  packageName   = 'zulu12'
   fileType      = 'msi'
   url64bit      = 'https://cdn.azul.com/zulu/bin/zulu12.1.3-ca-jdk12-win_x64.msi'
   checksum64    = '9556f4e65e9b54ea44b27c9d86cc14e0cdada15e85237671345c38a80018e4a4'
   checksumType64= 'sha256'
-  silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
+  silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
-  softwareName  = 'Zulu 11*'
+  softwareName  = 'Zulu 12*'
 }
 
 Install-ChocolateyPackage @packageArgs
