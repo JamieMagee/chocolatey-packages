372ca9de87a9aa2aa02ff7220fddce60ea9283b7
Jamie Magee <jamie.magee@gmail.com>
2019-08-06 11:13:47 +0200

    Update zulu au scripts

--- zulu9/update.ps1
+++ zulu9/update.ps1
@@ -1,26 +1,28 @@
 import-module au
 
-$releases = 'https://www.azul.com/downloads/zulu/zulu-windows/'
+$releases = 'https://www.azul.com/wp-admin/admin-ajax.php'
 
 function global:au_SearchReplace {
-   @{
-        ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-        }
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
     }
+  }
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+  $download_page = curl $releases -d 'latest=1&action=bundles_filter_query&action=search_bundles'
 
-    $url64   = $download_page.links | ? href -match '^.*jdk9.*.msi$' | % href | select -First 1
-    $version = (Split-Path $url64 -Leaf | Select-String -Pattern '^zulu9((\.\d+)+)').Matches.Value.Substring(4)
+  $release = (($download_page | ConvertFrom-Json) | Where-Object -FilterScript { $_.os_slug -eq "windows" -and $_.arch_slug -eq "x86-64-bit" -and $_.category_slug -eq "java-9" -and $_.packaging_slug -eq "jdk" } | Select-Object -Last 1).bundles | Where-Object {$_.extension -eq "msi"}
 
-    @{
-        URL64   = $url64
-        Version = $version
-    }
+  $url64 = $release.link
+  $version = (Split-Path $url64 -Leaf | Select-String -Pattern '^zulu9((\.\d+)+)').Matches.Value.Substring(4)
+
+  @{
+    URL64   = $url64
+    Version = $version
+  }
 }
 
 update -ChecksumFor 64
