bcce997d094ea417b6e74bd8b1400129cf58403c
Jamie Magee <jamie.magee@gmail.com>
2019-09-20 10:49:52 +0200

    Add zulu13

--- zulu13/tools/chocolateyInstall.ps1
+++ zulu13/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu12.3.11-ca-jdk12.0.2-win_x64.msi'
-  checksum64    = '6ef1335274ccb4d46d48bbc507ea48da8db68398d0ac25fe522298eee6f49cdf'
+  url64bit      = ''
+  checksum64    = ''
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
   validExitCodes= @(0)
