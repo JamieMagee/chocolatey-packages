e1befac5e6554132bd530fc9c42469213be3e32f
Chocolatey <chocolatey@realdimensions.net>
2020-08-01 12:51:50 +0000

    AU: 4 updated - zulu zulu11 zulu13 zulu14 [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/787bf96c961db98f6996403a6f3b0e77a5dcbed5

--- zulu/tools/chocolateyInstall.ps1
+++ zulu/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu13.31.11-ca-jdk13.0.3-win_x64.msi'
-  checksum64    = 'a54d96844aeb2ca01ac684e6fe740e36bc5322a96a958c013464fc57e54d8f02'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu14.29.23-ca-jdk14.0.2-win_x64.msi'
+  checksum64    = 'e5c30ed6923deecdb34b1c563302e2b802be1e4ff1ed63b53144dcaf0d43229d'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
   validExitCodes= @(0)
--- zulu13/tools/chocolateyInstall.ps1
+++ zulu13/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu13.29.9-ca-jdk13.0.2-win_x64.msi'
-  checksum64    = '0d64e1f7145e4640ec5e4e6242f68143b5cda13ddca85e9cd5be87755782db2a'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu13.33.25-ca-jdk13.0.4-win_x64.msi'
+  checksum64    = '5d4cbe16fd95043902bcefae24894a3a87543c295e98d0cb087a6917c5fe8af9'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
   validExitCodes= @(0)
