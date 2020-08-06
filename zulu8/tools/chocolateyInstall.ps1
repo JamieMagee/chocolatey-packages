1e80df2e83aa1a5a5d1ffac7bff7f2e9e21ff254
Chocolatey <chocolatey@realdimensions.net>
2020-08-06 00:24:07 +0000

    AU: 1 updated - zulu8 [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/9abf0679b75359e727e4962b470b2c2867bdb885

--- zulu8/tools/chocolateyInstall.ps1
+++ zulu8/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu8'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu8.48.0.51-ca-jdk8.0.262-win_x64.msi'
-  checksum64    = 'b472194fd6dcef99fb29f38c7aa5803f69778bb4f889bdd75abd820c7624930e'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu8.48.0.53-ca-jdk8.0.265-win_x64.msi'
+  checksum64    = 'b291a35afc72bfc942d4ca6dd639a1500f4fd1fec5bc38db986d6f7535b1a419'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
