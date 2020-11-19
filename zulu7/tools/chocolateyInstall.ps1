54cbef63f314b555c9821790766d271ffbd1ceb0
Chocolatey <chocolatey@realdimensions.net>
2020-11-19 00:24:07 +0000

    AU: 2 updated - zulu7 zulu8 [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/43f6ddd09c5952ea5fd0053fa4cce2a2456ad507

--- zulu7/tools/chocolateyInstall.ps1
+++ zulu7/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu7'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu7.42.0.13-ca-jdk7.0.282-win_x64.msi'
-  checksum64    = 'f6a532adba6529de72952b4a30433f530a4f6614115d1adac960600eb2782cff'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu7.42.0.51-ca-jdk7.0.285-win_x64.msi'
+  checksum64    = '410180f09114b56fe2b84a98177463735c3657efbc69d80ae058a888b9c63e5f'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
--- zulu8/tools/chocolateyInstall.ps1
+++ zulu8/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu8'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu8.50.0.21-ca-jdk8.0.272-win_x64.msi'
-  checksum64    = '544398b748013b8ce0b8b0c02c61e0aa5836cf38bc8e50eea98bcda9065b894c'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu8.50.0.53-ca-fx-jdk8.0.275-win_x64.msi'
+  checksum64    = 'c7ec1789efaa5064bd81d0354da936638fdcbb9fc75200804d753c10a9b47d5f'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
