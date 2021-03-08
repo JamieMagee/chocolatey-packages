2d029e1774909dccc83aef6a65abcec7cb71e9d8
Jamie Magee <jamie.magee@gmail.com>
2021-03-07 17:29:53 -0800

    zulu 15

---
+++ zulu15/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'zulu'
+  fileType      = 'msi'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu14.29.23-ca-jdk14.0.2-win_x64.msi'
+  checksum64    = 'e5c30ed6923deecdb34b1c563302e2b802be1e4ff1ed63b53144dcaf0d43229d'
+  checksumType64= 'sha256'
+  silentArgs    = '/qn /norestart  INSTALLDIR="C:\Program Files\Zulu\zulu"'
+  validExitCodes= @(0)
+  softwareName  = 'Zulu 15*'
+}
+
+Install-ChocolateyPackage @packageArgs
