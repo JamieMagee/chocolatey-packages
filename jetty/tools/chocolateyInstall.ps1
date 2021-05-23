be4b150fc1f0b205c257f3c76c5d8d8781fff86a
Chocolatey <chocolatey@realdimensions.net>
2021-05-23 00:49:10 +0000

    AU: 1 updated - jetty [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be

--- jetty/tools/chocolateyInstall.ps1
+++ jetty/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = 'jetty'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  url            = 'https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-home/11.0.2/jetty-home-11.0.2.zip'
-  checksum       = 'edaea1cde6875bc572e1e664a2f9c3e925d4f5273e51e01c8ee1e5e072bdae35'
+  url            = 'https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-home/11.0.3/jetty-home-11.0.3.zip'
+  checksum       = 'c4250b7aeb499218278fbb625296fc09ff45ffb4b00aaef547213ccfe02cff02'
   checksumType   = 'sha256'
 }
 
