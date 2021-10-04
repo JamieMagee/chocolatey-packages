bc1333cb392e3757aeaead5ed4e4f4361208cd59
Chocolatey <chocolatey@realdimensions.net>
2021-10-04 20:09:41 +0000

    AU: 1 updated - zulu17 [skip ci] https://gist.github.com/bab06468143f49adc9b0e65eb1e8c5be/0b70e2bbd92838c749a16f82b7e48bcefdbd9b9d

--- zulu17/tools/chocolateyInstall.ps1
+++ zulu17/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = 'zulu17'
   fileType      = 'msi'
-  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu17.28.13-ca-jdk17.0.0-win_x64.msi'
-  checksum64    = 'b87a006f4eb3c8401436cf3169c9746b940570257cf68bae25e47440870cb910'
+  url64bit      = 'https://cdn.azul.com/zulu/bin/zulu17.28.13-ca-fx-jdk17.0.0-win_x64.msi'
+  checksum64    = '238e29f4ebd74a85291bcb3d12bcc2a4a080c1da03df550dae10cc78f8dd7b4a'
   checksumType64= 'sha256'
   silentArgs    = '/qn /norestart'
   validExitCodes= @(0)
